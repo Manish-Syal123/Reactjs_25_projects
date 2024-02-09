d374c96126288f3caaac0d74281f70ace5c76ad4
Manish_Syal <manishsyal77@gmail.com>
2024-02-09 18:16:35 +0530

    Load More Products

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import Home from "./component/Accordian/Home";
 import RandomColor from "./component/random-color";
 import StarRating from "./component/Star-Rating";
 import ImageSlider from "./component/image-slider";
+import LoadMoreData from "./component/LoadMoreData";
+
 function App() {
   return (
     <div className="App">
@@ -14,11 +16,14 @@ function App() {
       {/* <StarRating noOfStars={10} /> */}
 
       {/* Image slider component */}
-      <ImageSlider
+      {/* <ImageSlider
         url={"https://picsum.photos/v2/list"}
         page={"1"}
         limit={"10"}
-      />
+      /> */}
+
+      {/* Loading more data */}
+      <LoadMoreData />
     </div>
   );
 }
